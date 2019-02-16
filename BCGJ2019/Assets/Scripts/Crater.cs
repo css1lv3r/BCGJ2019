1b6bfaa637a96721c5e0ec24437f81ea55c91b98
Mary C <marychung@live.com>
2019-02-16 14:11:42 -0800

    Added public fields to crater. isDry, growth (or alternatively a ready for harvest and ready for planting variable) will have to be accessed by player or person putting craters onto map. updateSprite is a bit redundant when following other procedures so maybe split up into different cases.

--- BCGJ2019/Assets/Scripts/Crater.cs
+++ BCGJ2019/Assets/Scripts/Crater.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Crater : MonoBehaviour
 {
+    private float update;
+    public bool isDry;
+    public int growth; // 0 for unplanted, 1 for immature plants, 2 for ready-to-harvest
+    public bool readyToHarvest;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (growth=1)
+        {
+            growth++;
+        }
+        updateSprite();      
+    }
+
+    void updateSprite()
+    {
+        if (isDry)
+        {
+            if (growth=0)
+            {
+                //update sprite to empty dry crater 
+            } else if (growth=1)
+            {
+                //update sprite to dry crater with buds
+            } else 
+            {
+                //update sprite to dry crater with mature plants
+            }
+        } else
+        {
+            if (growth=0)
+            {
+                //update sprite to empty wet crater
+            } else if (growth=1)
+            {
+                //update sprite to crater with immature algae
+            } else
+            {
+                //update sprite to crater with mature algae
+            }
+        }
         
     }
 }
