ab0bdf99428f26a2baca63f1edcc365bf18737ff
CS <hecklerofharbingers@gmail.com>
2019-02-16 17:45:33 -0800

    UI, basic interact collision detection, and new art assets

--- BCGJ2019/Assets/Scripts/Player.cs
+++ BCGJ2019/Assets/Scripts/Player.cs
@@ -6,43 +6,88 @@ using UnityEditor.Animations;
 public class Player : MonoBehaviour
 {
     public PlayerColor playerColor;
-
     public Animator animController;
+    public Transform overlapPosition;
+
+    public float SuitHealth
+    {
+        get { return suitHealth; }
+        set
+        {
+            suitHealth = value;
+            if (suitHealth > 100) { suitHealth = 100; }
+            if (suitHealth < 0) { suitHealth = 0; }
+        }
+    }
+
+    //------------------------------
 
     const float slowSpeed = 10f;
     const float medSpeed = 20f;
     const float normalSpeed =50f;
-
+    //------------------------------
     private float suitHealth;
     private int fuelInventory;
-
+    private int maxInventoryCapacity = 5;
 
 
     Vector2 normalizedDirection;
 
+    private List<GameObject> overlappingCraters = new List<GameObject>();
+    private GameObject[] allCraters;
+
+
+
 
     //----------------------------------------------------------------------------------------------------
-    // Start is called before the first frame update
 
-
-    void Start()
+    void Awake()
     {
         //animController = gameObject.GetComponentInChildren<Animator>();
+        allCraters = GameObject.FindGameObjectsWithTag("Crater");
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //get movement
         normalizedDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
-        Debug.Log(normalizedDirection);
+        //Debug.Log(normalizedDirection);
+
+        //get interact input
+        if (Input.GetKey(KeyCode.E))
+        {
+            UpdateOverlappingCraters();
+            Debug.Log(overlappingCraters.Count);
+        }
         
 
     }
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
+    //void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (col.gameObject.tag == "Crater")
+    //    {
+    //        if (!overlappingCraters.Contains(col.gameObject))
+    //        {
+    //            overlappingCraters.Add(col.gameObject);
+    //        }
+            
+    //    }
+    //}
 
-    }
+    //void OnTriggerExit2D(Collider2D col)
+    //{
+    //    if (col.gameObject.tag == "Crater")
+    //    {
+    //        if (overlappingCraters.Contains(col.gameObject))
+    //        {
+    //            overlappingCraters.Remove(col.gameObject);
+    //        }
+
+    //    }
+    //}
 
     void FixedUpdate()
     {
@@ -51,10 +96,14 @@ public class Player : MonoBehaviour
         gameObject.GetComponent<Rigidbody2D>().velocity = adjustedMoveVector ;
         //rotate character
         float moveDirection = (Mathf.Atan2(-normalizedDirection.x, normalizedDirection.y)*Mathf.Rad2Deg);
-        Debug.Log(moveDirection);
+        //Debug.Log(moveDirection);
 
         if (normalizedDirection != Vector2.zero) {
             transform.rotation = Quaternion.Euler(0, 0, moveDirection);
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0f;
         }
         
 
@@ -62,9 +111,42 @@ public class Player : MonoBehaviour
         animController.SetBool("isMoving",normalizedDirection!=Vector2.zero);
         //Debug.Log(animController.GetCurrentAnimatorClipInfo(0));
     }
+    //----------------------------------------------------------------------------------------------------
 
+     public bool TryToAddToInventory(int count)
+    {
+        if ((fuelInventory + count) > maxInventoryCapacity)
+        {
+            return false;
+        }
+        else
+        {
+            ++fuelInventory;
+            return true;
+        }
+
+    }
 
     //----------------------------------------------------------------------------------------------------
+
+    private bool IsInCrater()
+    {
+        return (overlappingCraters.Count > 0);
+    }
+
+    private void UpdateOverlappingCraters()
+    {
+        overlappingCraters.Clear();
+        Vector2 overlapPoint = new Vector2(overlapPosition.position.x, overlapPosition.position.y);
+        Debug.Log(allCraters.Length);
+        foreach(GameObject craterObject in allCraters)
+        {
+            if (craterObject.GetComponent<Collider2D>().OverlapPoint(overlapPoint))
+            {
+                overlappingCraters.Add(craterObject);
+            }
+        }
+    }
 }
 
 public enum PlayerColor { Red, Blue};
--- BCGJ2019/Assets/Scripts/SceneManager.cs
+++ BCGJ2019/Assets/Scripts/SceneManager.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    public static SceneManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private static SceneManager instance;
+
+    private void Awake()
+    {
+        if (instance == null) { instance = this; }
+    }
     // Start is called before the first frame update
     void Start()
     {
